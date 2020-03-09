1087a94afcc8119cfa8c99ec8a7c81c8089a7f7c
Hellowalls1 <thesingingbutcher@gmail.com>
2020-03-09 15:20:12 -0500

    got the selector bar working

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,13 @@
 import { getOfficers } from "./officers/OfficerProvider.js";
-
 import { CriminalsList } from "./criminals/CriminalList.js";
 import { getCriminals } from "./criminals/CriminalProvider.js";
+import { getConvictions } from "./convictions/ConvictionProvider.js";
+import ConvictionSelect from "./convictions/ConvictionSelect.js";
+getConvictions().then(ConvictionSelect)
 getCriminals().then(CriminalsList)
+
+
+
+
 
 getOfficers()
