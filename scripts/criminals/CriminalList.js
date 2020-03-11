bb858c18b30d0b5746f35ae77f1abb50437dbc48
Hellowalls1 <thesingingbutcher@gmail.com>
2020-03-11 14:45:24 -0500

    make dropdown list filter

--- scripts/criminals/CriminalList.js
+++ scripts/criminals/CriminalList.js
@@ -1,23 +1,42 @@
-import { Criminal } from "./Criminal.js";
-import { useCriminals } from "./CriminalProvider.js";
-useCriminals
 
+
+import { useCriminals } from "./CriminalProvider.js";
+import { Criminal } from "./Criminal.js";
 
 const contentTarget = document.querySelector(".criminalsContainer")
+const eventHub = document.querySelector(".container")
 
-export const CriminalsList = () => {
-
-    const criminalArray = useCriminals()
-        
+// Listen for the custom event you dispatched in ConvictionSelect
+eventHub.addEventListener("chosenCrime", event => {
 
 
-    criminalArray.forEach(criminalObject => { 
-   contentTarget.innerHTML += Criminal(criminalObject)
-    })
-//choosing object from array using .forEach() array method. Array method must contain for loop info
+    //get the criminals
+    const criminals = useCriminals()
+    //get the crime
+    const theCrimeThatWasChosen = event.detail.crime
+    
+   //look at all the criminals and figure out which one was the vandal
+   const guiltyCriminals = criminals.filter(criminal => {
+    if (criminal.conviction === theCrimeThatWasChosen) {
+        return true
+    }
+    return false
+   })
 
+
+    //clear inner HTML of the criminal list because
+    contentTarget.innerHTML = ""
+
+    // Build it up again
+    for (const singleCriminal of guiltyCriminals) {
+    contentTarget.innerHTML += Criminal(singleCriminal)
 }
+})
 
+export const CriminalList = () => {
+    const criminals = useCriminals()
 
-
-
+    for (const singleCriminal of criminals) {
+        contentTarget.innerHTML += Criminal(singleCriminal)
+    }
+}
